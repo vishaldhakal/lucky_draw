f2877e4c8bc73d1284e5c9f64b7371a4c0bf4d22
Manav Khadka <manavkhadka0@gmail.com>
2024-09-13 16:16:16 +0545

    Refactor URL patterns for offers module

--- offers/urls.py
+++ offers/urls.py
@@ -3,46 +3,109 @@ from . import views
 
 urlpatterns = [
     # Get Organization Details
-    path('get-organization/', views.GetOrganizationDetails.as_view(), name='get-organization-details'),
-    
+    path(
+        "get-organization/",
+        views.GetOrganizationDetails.as_view(),
+        name="get-organization-details",
+    ),
     # GiftItem URLs
-    path('gift-items/', views.GiftItemListCreateView.as_view(), name='gift-item-list-create'),
-    path('gift-items/<int:pk>/', views.GiftItemRetrieveUpdateDestroyView.as_view(), name='gift-item-detail'),
-
+    path(
+        "gift-items/",
+        views.GiftItemListCreateView.as_view(),
+        name="gift-item-list-create",
+    ),
+    path(
+        "gift-items/<int:pk>/",
+        views.GiftItemRetrieveUpdateDestroyView.as_view(),
+        name="gift-item-detail",
+    ),
     # LuckyDrawSystem URLs
-    path('lucky-draw-systems/', views.LuckyDrawSystemListCreateView.as_view(), name='luckydrawsystem-list-create'),
-    path('lucky-draw-systems/<int:pk>/', views.LuckyDrawSystemRetrieveUpdateDestroyView.as_view(), name='luckydrawsystem-detail'),
-
+    path(
+        "lucky-draw-systems/",
+        views.LuckyDrawSystemListCreateView.as_view(),
+        name="luckydrawsystem-list-create",
+    ),
+    path(
+        "lucky-draw-systems/<int:pk>/",
+        views.LuckyDrawSystemRetrieveUpdateDestroyView.as_view(),
+        name="luckydrawsystem-detail",
+    ),
     # RechargeCard URLs
-    path('recharge-cards/', views.RechargeCardListCreateView.as_view(), name='recharge-card-list-create'),
-    path('recharge-cards/<int:pk>/', views.RechargeCardRetrieveUpdateDestroyView.as_view(), name='recharge-card-detail'),
-
+    path(
+        "recharge-cards/",
+        views.RechargeCardListCreateView.as_view(),
+        name="recharge-card-list-create",
+    ),
+    path(
+        "recharge-cards/<int:pk>/",
+        views.RechargeCardRetrieveUpdateDestroyView.as_view(),
+        name="recharge-card-detail",
+    ),
     # IMEINO URLs
-    path('imei-numbers/', views.IMEINOListCreateView.as_view(), name='imeino-list-create'),
-    path('imei-numbers/<int:pk>/', views.IMEINORetrieveUpdateDestroyView.as_view(), name='imeino-detail'),
-
+    path(
+        "imei-numbers/", views.IMEINOListCreateView.as_view(), name="imeino-list-create"
+    ),
+    path(
+        "imei-numbers/<int:pk>/",
+        views.IMEINORetrieveUpdateDestroyView.as_view(),
+        name="imeino-detail",
+    ),
     # FixOffer URLs
-    path('fix-offers/', views.FixOfferListCreateView.as_view(), name='fix-offer-list-create'),
-    path('fix-offers/<int:pk>/', views.FixOfferRetrieveUpdateDestroyView.as_view(), name='fix-offer-detail'),
-
+    path(
+        "fix-offers/",
+        views.FixOfferListCreateView.as_view(),
+        name="fix-offer-list-create",
+    ),
+    path(
+        "fix-offers/<int:pk>/",
+        views.FixOfferRetrieveUpdateDestroyView.as_view(),
+        name="fix-offer-detail",
+    ),
     # MobileOfferType URLs
-    path('mobile-offer-condition/', views.MobileOfferConditionListCreateView.as_view(), name='mobile-offer-condition-list-create'),
-    path('mobile-offer-condition/<int:pk>/', views.MobileOfferConditionRetrieveUpdateDestroyView.as_view(), name='mobile-offer-condition-detail'),
-
+    path(
+        "mobile-offer-condition/",
+        views.MobileOfferConditionListCreateView.as_view(),
+        name="mobile-offer-condition-list-create",
+    ),
+    path(
+        "mobile-offer-condition/<int:pk>/",
+        views.MobileOfferConditionRetrieveUpdateDestroyView.as_view(),
+        name="mobile-offer-condition-detail",
+    ),
     # MobilePhoneOffer URLs
-    path('mobile-phone-offers/', views.MobilePhoneOfferListCreateView.as_view(), name='mobile-phone-offer-list-create'),
-    path('mobile-phone-offers/<int:pk>/', views.MobileOfferConditionRetrieveUpdateDestroyView.as_view(), name='mobile-phone-offer-detail'),
-
+    path(
+        "mobile-phone-offers/",
+        views.MobilePhoneOfferListCreateView.as_view(),
+        name="mobile-phone-offer-list-create",
+    ),
+    path(
+        "mobile-phone-offers/<int:pk>/",
+        views.MobilePhoneOfferRetrieveUpdateDestroyView.as_view(),
+        name="mobile-phone-offer-detail",
+    ),
     # RechargeCardOffer URLs
-    path('recharge-card-offers/', views.RechargeCardOfferListCreateView.as_view(), name='recharge-card-offer-list-create'),
-    path('recharge-card-offers/<int:pk>/', views.RechargeCardOfferRetrieveUpdateDestroyView.as_view(), name='recharge-card-offer-detail'),
-
+    path(
+        "recharge-card-offers/",
+        views.RechargeCardOfferListCreateView.as_view(),
+        name="recharge-card-offer-list-create",
+    ),
+    path(
+        "recharge-card-offers/<int:pk>/",
+        views.RechargeCardOfferRetrieveUpdateDestroyView.as_view(),
+        name="recharge-card-offer-detail",
+    ),
     # Customer URLs
-    path('customers/', views.CustomerListCreateView.as_view(), name='customer-list-create'),
-
+    path(
+        "customers/",
+        views.CustomerListCreateView.as_view(),
+        name="customer-list-create",
+    ),
     # Download Customer Detial
-    path('download-customer-details/', views.download_customers_detail, name='download-customer-details'),
-
+    path(
+        "download-customer-details/",
+        views.download_customers_detail,
+        name="download-customer-details",
+    ),
     # Upload IMEINo
-    path('upload-imeino/', views.UploadImeiBulk, name='upload-imeino'),
+    path("upload-imeino/", views.UploadImeiBulk, name="upload-imeino"),
 ]
