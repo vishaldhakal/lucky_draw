98aa2d3807cde9835f985f046abc20201146e560
vishaldhakal <vishaldhakal96@gmail.com>
2024-09-29 13:46:02 +0545

    Dfv

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from unfold.admin import ModelAdmin
 from .models import CustomUser, Organization
 
-@admin.register(CustomUser)
-class CustomUserAdmin(ModelAdmin):
-    list_display = ('email', 'role', 'organization', 'is_active')
-    list_filter = ('role', 'organization', 'is_active')
-    search_fields = ('email', 'organization__name')
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('email', 'role', 'organization')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('email', 'role', 'organization', 'is_active', 'is_staff', 'is_superuser')
+
+class CustomUserAdmin(ModelAdmin, UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ('email', 'role', 'organization', 'is_active', 'is_staff')
+    list_filter = ('role', 'organization', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('role', 'organization', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role', 'organization', 'is_active', 'is_staff')}
+        ),
+    )
+    search_fields = ('email',)
     ordering = ('email',)
 
-@admin.register(Organization)
-class OrganizationAdmin(ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
-    search_fields = ('name',)
-    ordering = ('name',)
+    def save_model(self, request, obj, form, change):
+        if not change:
+            # This is a new user, so set the password
+            obj.set_password(form.cleaned_data["password1"])
+        super().save_model(request, obj, form, change)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Organization)
