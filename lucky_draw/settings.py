e2ad3ac573092abeecc12c82105904446ed8d6e8
vishaldhakal <vishaldhakal96@gmail.com>
2025-09-30 14:48:10 +0545

    sdc

--- lucky_draw/settings.py
+++ lucky_draw/settings.py
@@ -79,17 +79,26 @@ WSGI_APPLICATION = 'lucky_draw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
-
+} """
 
 
 """ DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "lucky_draw",
+        "USER": "lucky_draw_admin",
+        "PASSWORD": "DatabaseUserPassword",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+} """
+
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "lucky_draw",
@@ -98,7 +107,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "",
     }
-} """
+}
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
